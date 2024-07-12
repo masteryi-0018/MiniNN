785089ad6d19c328aded8051a4ce07dded6fbe51
masteryi-0018 <1536474741@qq.com>
2024-07-12 20:03:09 +0800

    c and c++ head files

--- mininn/graph/tensor.cc
+++ mininn/graph/tensor.cc
@@ -1,15 +1,15 @@
 #include "mininn/graph/tensor.h"
 
-#include <stdlib.h>
+#include <cstdlib> // std::malloc
 #include <numeric> // std::accumulate
-#include <functional> // std::multiplies
+#include <functional> // std::multiplies, bazel needs
 
 Tensor::Tensor() {}
 
 void Tensor::set_shape(std::vector<int> shape) {
     shape_ = shape;
     size_ = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>());
-    buffer_ = malloc(size_);
+    buffer_ = std::malloc(size_);
 }
 
 std::vector<int> Tensor::get_shape() {
