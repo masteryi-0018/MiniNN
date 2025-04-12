68871fefa45e9b2b73c4c6ae2288efcd539b4cd3
masteryi-0018 <1536474741@qq.com>
2025-04-12 11:29:20 +0800

    mv2 passed and conv support dilations and group

--- mininn/kernel/conv_compute.cc
+++ mininn/kernel/conv_compute.cc
@@ -19,81 +19,87 @@ ConvCompute::~ConvCompute() {
 
 void conv_func(float* input_buffer, float* weight_buffer, float* bias_buffer, float* out_buffer,
                std::vector<int> dilations, std::vector<int> group, std::vector<int> kernel_shape,
-               std::vector<int> pads, std::vector<int> strides, std::vector<int> input_shape,
-               std::vector<int> weight_shape, std::vector<int> bias_shape, std::vector<int> out_shape) {
-    // todo: deal with group and dilations
-    // int batch_size = input_shape[0];
-    // int in_channels = input_shape[1];
-    // int input_height = input_shape[2];
-    // int input_width = input_shape[3];
+               std::vector<int> pads, std::vector<int> strides, std::vector<int> input_shape, std::vector<int> out_shape) {
+    int batch_size = input_shape[0];
+    int in_channels = input_shape[1];
+    int input_height = input_shape[2];
+    int input_width = input_shape[3];
 
-    // int out_channels = weight_shape[0];
-    // int output_height = out_shape[2];
-    // int output_width = out_shape[3];
+    int out_channels = out_shape[1];
+    int output_height = out_shape[2];
+    int output_width = out_shape[3];
 
-    // int kernel_height = kernel_shape[0];
-    // int kernel_width = kernel_shape[1];
+    int dilation_h = dilations[0];
+    int dilation_w = dilations[1];
 
-    // int padding_left = pads[0];
-    // int padding_right = pads[1];
-    // int padding_up = pads[2];
-    // int padding_down = pads[3];
+    int groups = group[0];
+    int in_group_size = in_channels / groups;
+    int out_group_size = out_channels / groups;
 
-    // int stride_height = strides[0];
-    // int stride_width = strides[1];
+    int kernel_height = kernel_shape[0];
+    int kernel_width = kernel_shape[1];
 
-    // // padding zero
-    // int padded_height = input_height + padding_up + padding_down;
-    // int padded_width = input_width + padding_left + padding_right;
-    // float* padded_input = new float[batch_size * in_channels * padded_height * padded_width];
-    // std::memset(padded_input, 0, batch_size * in_channels * padded_height * padded_width * sizeof(float));
+    int padding_left = pads[0];
+    int padding_right = pads[1];
+    int padding_up = pads[2];
+    int padding_down = pads[3];
 
-    // // cp input to padded_input
-    // for (int b = 0; b < batch_size; ++b) {
-    //     for (int c = 0; c < in_channels; ++c) {
-    //         for (int h = 0; h < input_height; ++h) {
-    //             for (int w = 0; w < input_width; ++w) {
-    //                 int padded_h = h + padding_up;
-    //                 int padded_w = w + padding_left;
-    //                 padded_input[b * in_channels * padded_height * padded_width +
-    //                              c * padded_height * padded_width +
-    //                              padded_h * padded_width +
-    //                              padded_w] =
-    //                     input_buffer[b * in_channels * input_height * input_width +
-    //                                  c * input_height * input_width +
-    //                                  h * input_width +
-    //                                  w];
-    //             }
-    //         }
-    //     }
-    // }
+    int stride_height = strides[0];
+    int stride_width = strides[1];
 
-    // // conv2d
-    // for (int b = 0; b < batch_size; ++b) {
-    //     for (int oc = 0; oc < out_channels; ++oc) {
-    //         for (int oh = 0; oh < output_height; ++oh) {
-    //             for (int ow = 0; ow < output_width; ++ow) {
-    //                 // compute one point of output
-    //                 float sum = 0.0f;
-    //                 int h_start = oh * stride_height;
-    //                 int w_start = ow * stride_width;
-    //                 for (int ic = 0; ic < in_channels; ++ic) {
-    //                     for (int kh = 0; kh < kernel_height; ++kh) {
-    //                         for (int kw = 0; kw < kernel_width; ++kw) {
-    //                             int ih = h_start + kh;
-    //                             int iw = w_start + kw;
-    //                             sum += padded_input[(b * in_channels + ic) * padded_height * padded_width + ih * padded_width + iw] *
-    //                                    weight_buffer[(oc * in_channels + ic) * kernel_height * kernel_width + kh * kernel_width + kw];
-    //                         }
-    //                     }
-    //                 }
-    //                 out_buffer[(b * out_channels + oc) * output_height * output_width + oh * output_width + ow] = sum;
-    //             }
-    //         }
-    //     }
-    // }
+    // padding zero
+    int padded_height = input_height + padding_up + padding_down;
+    int padded_width = input_width + padding_left + padding_right;
+    float* padded_input = new float[batch_size * in_channels * padded_height * padded_width];
+    std::memset(padded_input, 0, batch_size * in_channels * padded_height * padded_width * sizeof(float));
 
-    // delete[] padded_input;
+    // cp input to padded_input
+    for (int n = 0; n < batch_size; ++n) {
+        for (int c = 0; c < in_channels; ++c) {
+            for (int h = 0; h < input_height; ++h) {
+                for (int w = 0; w < input_width; ++w) {
+                    int padded_h_pos = h + padding_up;
+                    int padded_w_pos = w + padding_left;
+                    int src_idx = ((n * in_channels + c) * input_height + h) * input_width + w;
+                    int dst_idx = ((n * in_channels + c) * padded_height + padded_h_pos) * padded_width + padded_w_pos;
+                    padded_input[dst_idx] = input_buffer[src_idx];
+                }
+            }
+        }
+    }
+
+    // conv2d
+    for (int n = 0; n < batch_size; ++n) {
+        for (int g = 0; g < groups; ++g) {
+            for (int oc = 0; oc < out_group_size; ++oc) {
+                int out_c = g * out_group_size + oc;
+                for (int oh = 0; oh < output_height; ++oh) {
+                    for (int ow = 0; ow < output_width; ++ow) {
+                        float sum = bias_buffer[out_c];
+
+                        for (int ic = 0; ic < in_group_size; ++ic) {
+                            int in_c = g * in_group_size + ic;
+                            for (int kh = 0; kh < kernel_height; ++kh) {
+                                for (int kw = 0; kw < kernel_width; ++kw) {
+                                    int in_h_pos = oh * stride_height + kh * dilation_h;
+                                    int in_w_pos = ow * stride_width + kw * dilation_w;
+
+                                    int input_idx = ((n * in_channels + in_c) * padded_height + in_h_pos) * padded_width + in_w_pos;
+                                    int weight_idx = ((out_c * in_group_size + ic) * kernel_height + kh) * kernel_width + kw;
+                                    sum += padded_input[input_idx] * weight_buffer[weight_idx];
+                                }
+                            }
+                        }
+
+                        int out_idx = ((n * out_channels + out_c) * output_height + oh) * output_width + ow;
+                        out_buffer[out_idx] = sum;
+                    }
+                }
+            }
+        }
+    }
+
+    delete[] padded_input;
 }
 
 void ConvCompute::run() {
@@ -112,8 +118,6 @@ void ConvCompute::run() {
     std::vector<int> strides = params->strides;
 
     std::vector<int> input_shape = input->get_shape();
-    std::vector<int> weight_shape = weight->get_shape();
-    std::vector<int> bias_shape = bias->get_shape();
     std::vector<int> out_shape = out->get_shape();
     float* input_buffer = reinterpret_cast<float*>(input->get_buffer());
     float* weight_buffer = reinterpret_cast<float*>(weight->get_buffer());
@@ -121,8 +125,7 @@ void ConvCompute::run() {
     float* out_buffer = reinterpret_cast<float*>(out->get_buffer());
 
     conv_func(input_buffer, weight_buffer, bias_buffer, out_buffer, 
-              dilations, group, kernel_shape, pads, strides,
-              input_shape, weight_shape, bias_shape, out_shape);
+              dilations, group, kernel_shape, pads, strides, input_shape, out_shape);
 
     auto end_time = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_seconds = end_time - start_time;
