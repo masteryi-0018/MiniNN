e8c5d7b19b936ba1495a5be2e807419bd7178785
masteryi-0018 <1536474741@qq.com>
2025-05-27 18:40:41 +0800

    try to add glog

--- mininn/test/gtest_main.cc
+++ mininn/test/gtest_main.cc
@@ -2,8 +2,17 @@
 #include "mininn/graph/use_kernel.h"
 
 #include <gtest/gtest.h>
+// #include <glog/logging.h>
 
 int main(int argc, char** argv) {
+    // FLAGS_logtostderr  = 1;
+    // google::InitGoogleLogging(argv[0]);
+    // LOG(INFO) << "Found " << 1 << " cookies";
+    // LOG(WARNING) << "Found " << 2 << " cookies";
+    // LOG(ERROR) << "Found " << 3 << " cookies";
+    // LOG(FATAL) << "Found " << 4 << " cookies";
+    // return 0;
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
