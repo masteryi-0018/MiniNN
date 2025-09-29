5f581c9a36bd7177d40f1ee0e62248302cb24ae0
masteryi-0018 <1536474741@qq.com>
2025-09-29 20:33:08 +0800

    py add ut of add

---
+++ python/test/unit_test/add_test.py
@@ -0,0 +1,51 @@
+import onnx
+from common import convert_model, test_model
+from onnx import TensorProto, helper
+
+
+def make_model(input_shape=[1, 3, 224, 224], output_shape=[1, 3, 224, 224]):
+    input0 = helper.make_tensor_value_info("input0", TensorProto.FLOAT, input_shape)
+
+    input1 = helper.make_tensor_value_info("input1", TensorProto.FLOAT, input_shape)
+
+    output0 = helper.make_tensor_value_info("output0", TensorProto.FLOAT, output_shape)
+
+    add_node = helper.make_node(
+        "Add",
+        inputs=["input0", "input1"],
+        outputs=["output0"],
+        name="add_node",
+    )
+
+    graph = helper.make_graph([add_node], "add_model", [input0, input1], [output0])
+
+    model = helper.make_model(
+        graph,
+        producer_name="onnx-mininn",
+        opset_imports=[helper.make_opsetid("", 10)],  # means the imports
+        ir_version=6,  # means the format
+    )
+
+    onnx.checker.check_model(model)
+
+    model_path = "models/add_model.onnx"
+    onnx.save(model, model_path)
+    print(f"模型已保存为 {model_path}")
+
+    return model_path
+
+
+if __name__ == "__main__":
+
+    batch_size = 1
+    channel = 3
+    height = 224
+    width = 224
+    input_shape = (batch_size, channel, height, width)
+    output_shape = (batch_size, channel, height, width)
+
+    model_path = make_model(input_shape=input_shape, output_shape=output_shape)
+
+    new_model_path = convert_model(model_path)
+
+    test_model(new_model_path, model_path, True)
